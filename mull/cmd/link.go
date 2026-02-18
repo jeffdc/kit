3eaa7a0d14c8229eafa098821cd722873bb542e0
jeffdc <jeff@nothoo.com>
2026-02-18 12:44:09 -0500

    Support multiple target IDs in link command

--- mull/cmd/link.go
+++ mull/cmd/link.go
@@ -8,25 +8,35 @@ import (
 )
 
 var linkCmd = &cobra.Command{
-	Use:   "link <id> <type> <id>",
-	Short: "Create a relationship between two matters",
+	Use:   "link <id> <type> <id> [id...]",
+	Short: "Create a relationship between matters",
 	Long:  `Type is one of: relates, blocks, needs, parent.`,
-	Args:  cobra.ExactArgs(3),
+	Args:  cobra.MinimumNArgs(3),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		id1 := args[0]
 		relType := args[1]
-		id2 := args[2]
+		targets := args[2:]
 
-		if err := store.LinkMatters(id1, relType, id2); err != nil {
-			return err
-		}
-
-		return json.NewEncoder(os.Stdout).Encode(map[string]any{
-			"linked": map[string]string{
+		results := make([]map[string]string, 0, len(targets))
+		for _, id2 := range targets {
+			if err := store.LinkMatters(id1, relType, id2); err != nil {
+				return err
+			}
+			results = append(results, map[string]string{
 				"from": id1,
 				"type": relType,
 				"to":   id2,
-			},
+			})
+		}
+
+		// Backward compatible: single target returns single object
+		if len(results) == 1 {
+			return json.NewEncoder(os.Stdout).Encode(map[string]any{
+				"linked": results[0],
+			})
+		}
+		return json.NewEncoder(os.Stdout).Encode(map[string]any{
+			"linked": results,
 		})
 	},
 }
